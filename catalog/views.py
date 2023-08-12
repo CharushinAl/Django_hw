33b787d275e47e4ef8ab895ab8e6850f151d0bc8
Aleksandr Charushin <charushinas@gmail.com>
2023-08-12 21:40:29 +0300

    add

---
+++ catalog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "catalog/index.html")
+
+
+def home(request):
+    return render(request, "catalog/home.html")
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        number = request.POST.get('number')
+        message = request.POST.get('message')
+        print(f'{name} {number}: {message}')
+    return render(request, "catalog/contacts.html")
