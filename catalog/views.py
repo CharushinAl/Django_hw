24162d6959dfad8389e27c56adc92de58c48d012
Aleksandr Charushin <charushinas@gmail.com>
2023-08-14 08:43:43 +0300

    Changed urlpatterns. Deleted controller index

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "catalog/index.html")
-
-
 def home(request):
     return render(request, "catalog/home.html")
 
